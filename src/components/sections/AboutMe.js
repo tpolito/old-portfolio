ec895d19f80a39736463ee3be2a13cf5bb345629
TPolito <0613.tyler@gmail.com>
2020-04-13 11:43:37 -0500

    updated projects, added image to about me

--- src/components/sections/AboutMe.js
+++ src/components/sections/AboutMe.js
@@ -1,28 +1,29 @@
 import React from 'react';
+import aboutMe from './about-me-image.png';
 
 const AboutMe = () => {
   return (
-    <section id="aboutMe" className="aboutMe-section hero-is-fullheight">
-      <div className="container viewport-100">
-        <p className="is-size-2 has-text-centered text-banner">
-          <span className="text-highlight">About Me</span>
+    <section id='aboutMe' className='aboutMe-section hero-is-fullheight'>
+      <div className='container viewport-100'>
+        <p className='is-size-2 has-text-centered text-banner'>
+          <span className='text-highlight'>About Me</span>
         </p>
-        <div className="columns is-vcentered">
-          <div className="column">
-            <p className="is-size-5 has-text-justified">
+        <div className='columns is-vcentered'>
+          <div className='column'>
+            <p className='is-size-5 has-text-justified'>
               I am fresh out of college, and looking to work on projects that
               can teach me new things.
             </p>
             <br />
-            <p className="is-size-5">
+            <p className='is-size-5'>
               I enjoy developing websites & games, rockclimbing and table top
               games.
             </p>
             <br />
-            <p className="is-size-5">I have a cat named Mila</p>
+            <p className='is-size-5'>I have a cat named Mila</p>
           </div>
-          <div className="column">
-            <img src="https://via.placeholder.com/400x600" alt="my self" />
+          <div className='column'>
+            <img src={aboutMe} alt='my self' />
           </div>
         </div>
       </div>
--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -1,76 +1,105 @@
 import React from 'react';
 import dndToolkit from './dnd-toolkit.png';
-import poeStashspy from './poe-stashspy.png';
+import gameOfLife from './game-of-life.png';
+import discordBot from './discord-bot.png';
 
 const Projects = () => {
   return (
-    <section id="projects" className="projects-section hero-is-fullheight">
-      <div className="container viewport-100">
-        <p className="is-size-2 has-text-centered text-banner">
-          <span className="text-highlight">My Projects</span>
+    <section id='projects' className='projects-section hero-is-fullheight'>
+      <div className='container viewport-100'>
+        <p className='is-size-2 has-text-centered text-banner'>
+          <span className='text-highlight'>My Projects</span>
         </p>
-        <div className="columns is-vcentered">
-          <div className="column">
-            <figure className="image is-square project-img-fig">
+        <div className='columns is-vcentered'>
+          <div className='column'>
+            <figure className='image is-square project-img-fig'>
               <a
-                href="https://dndtoolkit.app/"
-                target="_blank"
-                rel="noopener noreferrer"
+                href='https://dndtoolkit.app/'
+                target='_blank'
+                rel='noopener noreferrer'
               >
                 <img
                   src={dndToolkit}
-                  className="is-hidden-mobile is-rounded"
-                  alt="project thumbnail"
+                  className='is-hidden-mobile is-rounded'
+                  alt='project thumbnail'
                 />
               </a>
             </figure>
-            <p className="project-img-caption is-size-5">
+            <p className='project-img-caption is-size-5'>
               <a
-                href="https://dndtoolkit.app/"
+                href='https://dndtoolkit.app/'
                 style={{ color: 'white' }}
-                target="_blank"
-                rel="noopener noreferrer"
+                target='_blank'
+                rel='noopener noreferrer'
               >
                 DnD Toolkit
               </a>
             </p>
-            <div class="tags">
-              <span class="tag is-info">React</span>
-              <span class="tag is-warning">JavaScript</span>
-              <span class="tag is-primary">MaterializeCSS</span>
-              <span class="tag is-link">API's</span>
+            <div className='tags'>
+              <span className='tag is-info'>React</span>
+              <span className='tag is-warning'>JavaScript</span>
+              <span className='tag is-primary'>MaterializeCSS</span>
+              <span className='tag is-link'>API's</span>
             </div>
           </div>
-          <div className="column">
-            <figure className="image is-square project-img-fig">
-              <img
-                src={poeStashspy}
-                className="is-hidden-mobile is-rounded"
-                alt="project thumbnail"
-              />
+          <div className='column'>
+            <figure className='image is-square project-img-fig'>
+              <a
+                href='https://github.com/tpolito/rainbow-game-of-life'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <img
+                  src={gameOfLife}
+                  className='is-hidden-mobile is-rounded'
+                  alt='project thumbnail'
+                />
+              </a>
             </figure>
-            <p className="project-img-caption is-size-5">
-              Path of Exile Stash Spy
-            </p>
-            <div class="tags">
-              <span class="tag is-info">React</span>
-              <span class="tag is-warning">JavaScript</span>
-              <span class="tag is-link">API's</span>
-              <span class="tag is-primary">SASS</span>
+            <a
+              href='https://github.com/tpolito/rainbow-game-of-life'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <p className='project-img-caption is-size-5'>
+                Rainbow Game of Life
+              </p>
+            </a>
+
+            <div className='tags'>
+              <span className='tag is-info'>React</span>
+              <span className='tag is-link'>TypeScript</span>
+              <span className='tag is-success'>Algo</span>
             </div>
           </div>
-          <div className="column">
-            <figure className="image is-square project-img-fig">
-              <img
-                src="https://via.placeholder.com/432x432"
-                className="is-hidden-mobile is-rounded"
-                alt="project thumbnail"
-              />
+          <div className='column'>
+            <figure className='image is-square project-img-fig'>
+              <a
+                href='https://github.com/tpolito/discord-bot'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <img
+                  src={discordBot}
+                  className='is-hidden-mobile is-rounded'
+                  alt='project thumbnail'
+                />
+              </a>
             </figure>
-            <p className="project-img-caption is-size-5">Platformer Game</p>
-            <div class="tags">
-              <span class="tag is-light">C#</span>
-              <span class="tag is-success">Godot</span>
+            <a
+              href='https://github.com/tpolito/discord-bot'
+              style={{ color: 'white' }}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <p className='project-img-caption is-size-5'>
+                Custom Discord Bot
+              </p>
+            </a>
+            <div className='tags'>
+              <span className='tag is-warning'>JavaScript</span>
+              <span className='tag is-link'>API's</span>
+              <span className='tag is-success'>NodeJS</span>
             </div>
           </div>
         </div>
